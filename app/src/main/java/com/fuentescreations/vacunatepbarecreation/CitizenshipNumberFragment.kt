617594a66b48348df8dfcddf1683d9363e50baf2
Gonza <gfuentes@pdssa.com.ar>
2021-09-09 22:23:07 -0300

    Changes from laptop

--- app/src/main/java/com/fuentescreations/vacunatepbarecreation/CitizenshipNumberFragment.kt
+++ app/src/main/java/com/fuentescreations/vacunatepbarecreation/CitizenshipNumberFragment.kt
@@ -1,10 +1,15 @@
 package com.fuentescreations.vacunatepbarecreation
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
 import com.fuentescreations.vacunatepbarecreation.databinding.FragmentCitizenshipNumberBinding
 
 class CitizenshipNumberFragment : Fragment(R.layout.fragment_citizenship_number){
@@ -15,5 +20,35 @@ class CitizenshipNumberFragment : Fragment(R.layout.fragment_citizenship_number)
         super.onViewCreated(view, savedInstanceState)
 
         b = FragmentCitizenshipNumberBinding.bind(view)
+
+        b.lyTramitNumberCitizenshipNumber.setOnClickListener {
+            AlertDialog.Builder(requireContext())
+                .setView(
+                    ImageView(requireContext()).apply {
+                        this.setImageResource(R.drawable.dni)
+                        this.adjustViewBounds = true
+                    })
+                .setPositiveButton("Ok") { d, _ ->
+                    d.dismiss()
+                }
+                .show()
+        }
+
+
+
+        b.tilCitizenshipNumber.setEndIconOnClickListener {
+            Toast.makeText(requireContext(), "Click!!", Toast.LENGTH_SHORT).show()
+        }
+
+        b.etCitizenshipNumber.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                b.btnSentCitizenshipNumber.isEnabled = p0?.length==10
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+
+        })
     }
 }
