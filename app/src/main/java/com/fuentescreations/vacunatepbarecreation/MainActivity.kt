b89029e3c19f3ff6b543ba5cb750ad8653eabeb7
Gonza <gfuentes@pdssa.com.ar>
2021-09-18 02:30:16 -0300

    Changes from Laptop

--- app/src/main/java/com/fuentescreations/vacunatepbarecreation/MainActivity.kt
+++ app/src/main/java/com/fuentescreations/vacunatepbarecreation/MainActivity.kt
@@ -2,10 +2,19 @@ package com.fuentescreations.vacunatepbarecreation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fuentescreations.vacunatepbarecreation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var b: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
+
+        b.topAppBar.setNavigationOnClickListener {
+            b.drawerLayout.open()
+        }
     }
 }
