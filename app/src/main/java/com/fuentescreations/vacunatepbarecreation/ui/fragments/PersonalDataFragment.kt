2c9762687994667622cfadd09a387bbf709d1d9a
Gonza <gfuentes@pdssa.com.ar>
2021-10-03 01:34:04 -0300

    Changes

--- app/src/main/java/com/fuentescreations/vacunatepbarecreation/ui/fragments/PersonalDataFragment.kt
+++ app/src/main/java/com/fuentescreations/vacunatepbarecreation/ui/fragments/PersonalDataFragment.kt
@@ -1,14 +1,32 @@
 package com.fuentescreations.vacunatepbarecreation.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
 import android.view.View
-import android.view.ViewGroup
 import com.fuentescreations.vacunatepbarecreation.R
+import com.fuentescreations.vacunatepbarecreation.databinding.BottomSheetDialogIdentityBinding
+import com.fuentescreations.vacunatepbarecreation.databinding.FragmentPersonalDataBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class PersonalDataFragment : Fragment(R.layout.fragment_personal_data) {
+
+    private lateinit var b: FragmentPersonalDataBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        b = FragmentPersonalDataBinding.bind(view)
+
+        b.etIdentity.setOnClickListener {
+            setupBottomSheetDialog()
+        }
+    }
+
+    private fun setupBottomSheetDialog() {
+        val binding = BottomSheetDialogIdentityBinding.inflate(LayoutInflater.from(requireContext()))
+        val dialog = BottomSheetDialog(requireContext())
+        dialog.setContentView(binding.root)
+
+        dialog.show()
     }
 }
